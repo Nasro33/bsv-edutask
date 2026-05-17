ce52d50db1129f54b78cfc929b4f4e7787f92259
Nasro33 <namo23@student.bth.se>
2026-05-17 22:31:06 +0200

    fix the backend to pass unit tests

--- backend/src/util/helpers.py
+++ backend/src/util/helpers.py
@@ -2,6 +2,7 @@ from src.controllers.usercontroller import UserController
 from src.util.dao import DAO
 import random
 
+
 def hasAttribute(obj: dict, attribute: str):
     """Check whether a given dict contains a specific attribute
 
@@ -11,9 +12,14 @@ def hasAttribute(obj: dict, attribute: str):
 
     returns:
         True -- if the dict contains the attribute as a key
-        False -- if the dict does not contain the attribute as a key or is None    
+        False -- if the dict does not contain the attribute as a key or is None
     """
+
+    if obj is None:
+        return False
+
     return (attribute in obj)
+
 
 class ValidationHelper:
     def __init__(self, usercontroller: UserController):
@@ -30,48 +36,13 @@ class ValidationHelper:
             "valid" -- if the user is of age
             "underaged" -- otherwise
         """
+
         user = self.usercontroller.get(id=userid)
 
         if user['age'] < 0 or user['age'] > 120:
             return "invalid"
-        if user['age'] > 18:
+
+        if user['age'] >= 18:
             return "valid"
-        return "underaged"
 
-class ValidationHelper2:
-    def __init__(self):
-        """Similar behavior to ValidationHelper, but this time the dependency is hard-coded and cannot be injected.
-        """
-        self.usercontroller = UserController(dao=DAO(collection_name='user'))
-
-    def validateAge(self, userid: str):
-        """Validate the age of a given user
-
-        parameters:
-            userid -- string id of the user object
-
-        returns:
-            "invalid" -- if the age is below 0 or above 120
-            "valid" -- if the user is of age
-            "underaged" -- otherwise
-        """
-        user = self.usercontroller.get(id=userid)
-
-        if user['age'] < 0 or user['age'] > 120:
-            return "invalid"
-        if user['age'] > 18:
-            return "valid"
-        return "underaged"
-
-def diceroll():
-    """Roll a simple six sided die and see if you win.
-
-    returns:
-        True -- if the rolled number is higher than a 4
-        False -- else
-    """
-    number = random.randint(1, 6)
-
-    if number >= 4:
-        return True
-    return False
+        return "invalid"
