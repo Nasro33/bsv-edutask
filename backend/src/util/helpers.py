e7e1e5b4e6b119981e56e3d27ac3ac41a0d01768
Nasro33 <namo23@student.bth.se>
2026-05-17 22:33:47 +0200

    fix helper imports and logic

--- backend/src/util/helpers.py
+++ backend/src/util/helpers.py
@@ -4,21 +4,12 @@ import random
 
 
 def hasAttribute(obj: dict, attribute: str):
-    """Check whether a given dict contains a specific attribute
-
-    parameters:
-        obj -- a dict object
-        attribute -- the key which potentially occurs in the obj dict
-
-    returns:
-        True -- if the dict contains the attribute as a key
-        False -- if the dict does not contain the attribute as a key or is None
-    """
+    """Check whether a given dict contains a specific attribute"""
 
     if obj is None:
         return False
 
-    return (attribute in obj)
+    return attribute in obj
 
 
 class ValidationHelper:
@@ -26,16 +17,6 @@ class ValidationHelper:
         self.usercontroller = usercontroller
 
     def validateAge(self, userid: str):
-        """Validate the age of a given user
-
-        parameters:
-            userid -- string id of the user object
-
-        returns:
-            "invalid" -- if the age is below 0 or above 120
-            "valid" -- if the user is of age
-            "underaged" -- otherwise
-        """
 
         user = self.usercontroller.get(id=userid)
 
@@ -45,4 +26,40 @@ class ValidationHelper:
         if user['age'] >= 18:
             return "valid"
 
-        return "invalid"
+        return "underaged"
+
+
+class ValidationHelper2:
+    def __init__(self):
+        """
+        Similar behavior to ValidationHelper,
+        but dependency is hard-coded.
+        """
+        self.usercontroller = UserController(
+            dao=DAO(collection_name='user')
+        )
+
+    def validateAge(self, userid: str):
+
+        user = self.usercontroller.get(id=userid)
+
+        if user['age'] < 0 or user['age'] > 120:
+            return "invalid"
+
+        if user['age'] >= 18:
+            return "valid"
+
+        return "underaged"
+
+
+def diceroll():
+    """
+    Roll a simple six sided die and see if you win.
+    """
+
+    number = random.randint(1, 6)
+
+    if number >= 5:
+        return True
+
+    return False
